729e599eafb875fcc3ff86ce4ea138f339559624
kjlee88 <53717662+kjlee88@users.noreply.github.com>
2022-02-23 14:24:15 -0500

    Update Login.java
    Action behavior added as comment.

--- src/controller/Login.java
+++ src/controller/Login.java
@@ -1,4 +1,10 @@
 package controller;
 
 public class Login {
+
+/** text field id and password is saved to string variable. Search ID against sql database to find linked password, save it to another variable.
+User input password is checked against saved variable. Successful attempt triggers message "Login successful" and calls the Menu screen.
+When ID is not found, msg "ID not found". When ID is found but password is wrong, msg "Incorrect password". Each login attempt logs. **/
+
+
 }
