c92672c7a6376def41bf9c8b6792b68c0bbca64a
kyunglee <klee216@wgu.edu>
2022-02-23 05:42:50 -0500

    GUI done

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,9 +11,9 @@ public class Main extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/view/login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/view/appointment.fxml"));
         primaryStage.setTitle("Scheduler");
-        primaryStage.setScene(new Scene(root, 300, 200));
+        primaryStage.setScene(new Scene(root, 1160, 500));
         primaryStage.show();
     }
 
